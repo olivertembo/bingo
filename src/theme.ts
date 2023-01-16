f7892d8592459aeadf8c0fad08e75d8f7dc12c2f
oliver-distributed <tembokoliver3@gmail.com>
2023-01-16 07:32:04 +0100

    Initial commit

---
+++ src/theme.ts
@@ -0,0 +1,38 @@
+import { Roboto } from '@next/font/google';
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 300,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+  palette: {
+    primary: {
+      main: '#1e989d',
+    },
+    secondary: {
+      main: '#ce2641',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
+
+export default theme;
